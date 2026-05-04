af8e3143380f6ecbb6f5eed1e038c504e780d322
tung.home <tung.l@vanthiel.com>
2026-05-04 18:44:13 +0700

    x

--- src/features/K/Components/KDialog.tsx
+++ src/features/K/Components/KDialog.tsx
@@ -14,12 +14,12 @@ import { Button } from "@/shared";
 import { GenericTextField, IconPicker } from "@/shared";
 import { useKNodeDialogHelper as useKFolderDialogHelper } from "../hooks/useKNodeDialog.helper";
 import { GenericAutoComplete, type IAutoCompleteOptions } from "@/shared";
-import {useKStore} from "../store/K.store";
-import {isFolder, KItemV2} from "../types/K-v2.types";
+import {useKStore} from "../store/useK.store";
 import {kconstants} from "../utils/K.Constants";
-import {useNodeDialogStore} from "../store/KNodeDialog.store";
+import {useNodeDialogStore} from "../store/useKNodeDialog.store";
 import { getIconDefaultColor } from "@/shared";
 import { useGlobalShortcut } from "@/shared";
+import {isFolder, KItemV2} from "../types/kV2.type";
 
 export function KDialog() {
     // Get state from ExplorerStore
